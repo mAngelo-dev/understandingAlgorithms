0307bd2213018faa82cd94415f57af279f1cc92c
mAngelo-dev <m.angelodev@gmail.com>
2026-02-24 13:56:52 -0300

    Remove debugging console.log from findBiggestNumberInArray in understandingAlgo/4/3

--- src/understandingAlgo/4/3/index.ts
+++ src/understandingAlgo/4/3/index.ts
@@ -3,5 +3,3 @@ function findBiggestNumberInArray(arr: Array<number>): number {
     const max = arr[0];
     return max > findBiggestNumberInArray(arr.slice(1)) ? max : findBiggestNumberInArray(arr.slice(1));
 }
-
-console.log(findBiggestNumberInArray([1, 200, 3, 43098, 522]))
